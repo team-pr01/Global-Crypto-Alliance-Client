f3ebe7145ec69ef92aecb67bfa53de87dc0bfe74
rahulsd380 <rahulsd380@gmail.com>
2025-07-22 09:44:24 +0600

    Initial project setup without tailwind configuration

---
+++ src/app/(authLayout)/layout.tsx
@@ -0,0 +1,10 @@
+
+const AuthLayout = ({children} : {children: React.ReactNode}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default AuthLayout;
---
+++ src/app/(mainLayout)/layout.tsx
@@ -0,0 +1,10 @@
+
+const MainLayout = ({children} : {children: React.ReactNode}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default MainLayout;
