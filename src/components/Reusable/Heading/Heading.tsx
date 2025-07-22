86860f75f135ab992eb16ad988704b4f3075f8bc
prernabadwane <prernabadwane@gmail.com>
2025-07-22 13:47:54 +0530

    about uscompleted

---
+++ src/components/Reusable/Heading/Heading.tsx
@@ -0,0 +1,23 @@
+interface ReusableHeadingProps {
+  highlight: string;
+  className?: string;
+}
+
+const ReusableHeading: React.FC<ReusableHeadingProps> = ({ highlight, className }) => {
+  return (
+    <h2 className={`text-xl md:text-[40px] text-center md:text-start font-bold text-white ${className}`}>
+  <span
+    className="text-transparent bg-clip-text"
+    style={{
+      backgroundImage: "linear-gradient(90deg, #FF7800 51.71%, #FFE601 100%)",
+    }}
+  >
+    {highlight}
+  </span>{" "}
+
+</h2>
+
+  );
+};
+
+export default ReusableHeading;
