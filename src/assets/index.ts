3e81604b0b5cfe1c17d3088fbdf8fcfb0507335a
rahulsd380 <rahulsd380@gmail.com>
2025-07-23 09:45:25 +0600

    Added guture roadmap setion

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,42 +4,43 @@ import telegram from "../assets/icons/telegram.svg";
 import whatsapp from "../assets/icons/whats-app.svg";
 import youtube from "../assets/icons/youtube.svg";
 import facebook from "../assets/icons/facebook.svg";
-import gMeet from "../assets/icons/g-meet.svg"
-import wallet from "../assets/icons/wallet.svg"
-
+import gMeet from "../assets/icons/g-meet.svg";
+import wallet from "../assets/icons/wallet.svg";
+import telegramIcon from "../assets/icons/telegram-icon.svg";
 
 // gradient bg
 import accountPreview from "./gradient-bg/accout-preview.svg";
 
-
 // images
 // import comingSoonPage from "../assets/images/02 - Distance Nebulae.png";
-import layoutBg from "../assets/images/layout-bg.png"
-import heroBg from "../assets/images/hero-bg.png"
-import heroBgSmall from "../assets/images/hero-bg-small.png"
-import logo from "../assets/images/logo.png"
-import companyNameLogo from "../assets/images/company-name-logo.png"
-import heroImage from "../assets/images/hero-image.png"
-import heroImage2 from "../assets/images/hero-image-2.png"
-import coinImage from "../assets/images/coin-image.png"
-import buttonDesign from "../assets/images/btn-design.svg"
-
-
-
-
-
+import layoutBg from "../assets/images/layout-bg.png";
+import heroBg from "../assets/images/hero-bg.png";
+import heroBgSmall from "../assets/images/hero-bg-small.png";
+import logo from "../assets/images/logo.png";
+import companyNameLogo from "../assets/images/company-name-logo.png";
+import heroImage from "../assets/images/hero-image.png";
+import heroImage2 from "../assets/images/hero-image-2.png";
+import coinImage from "../assets/images/coin-image.png";
+import buttonDesign from "../assets/images/btn-design.svg";
+import pdf from "../assets/images/pdf.png";
+import gradientBottomLeft from "../assets/images/gradient-bottom-left.svg";
+import gradientTopRight from "../assets/images/gradient-top-right.svg";
+import linnerBg from "../assets/images/linner-bg.png";
 
 export const IMAGES = {
- layoutBg,
- logo,
- companyNameLogo,
- heroImage,
- heroBg,
- heroBgSmall,
- heroImage2,
- coinImage,
- buttonDesign
-
+  layoutBg,
+  logo,
+  companyNameLogo,
+  heroImage,
+  heroBg,
+  heroBgSmall,
+  heroImage2,
+  coinImage,
+  buttonDesign,
+  pdf,
+  gradientBottomLeft,
+  gradientTopRight,
+  linnerBg,
 };
 
 export const ICONS = {
@@ -48,12 +49,9 @@ export const ICONS = {
   facebook,
   youtube,
   gMeet,
-  wallet
+  wallet,
+  telegramIcon,
 };
 export const GRADIENT_BG = {
- accountPreview
+  accountPreview,
 };
-
-// export const ANIMATEDICONS = {
-// accountPreview
-// };
